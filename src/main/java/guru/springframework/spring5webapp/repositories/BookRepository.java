58e03b173be281563dd0950947dc6aad0b78e861
Rajanikanth007 <rnvrk.2015@gmail.com>
2020-11-07 19:37:30 +0530

    Till Lesson 21 completed.
    thymeleaf resources needs to add to work this code correctly. only new
    file created for BookController.java. other files empty lines are
    removed.

--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -1,7 +1,6 @@
 package guru.springframework.spring5webapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
 import guru.springframework.spring5webapp.domain.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
